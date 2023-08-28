94ca174c0b2bea33ca5b3791b41fa826991fc240
dErnandoV7 <d.ernandov7@gmail.com>
2023-08-28 20:34:42 -0300

    projeto calculadora

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function insert(num) {
+    let resultado = document.querySelector('#resultado');
+    resultado.innerHTML = resultado.innerHTML + num;
+};
+
+function clean() {
+    document.querySelector('#resultado').innerHTML = '';
+}
+
+function back() {
+    let resultado = document.querySelector('#resultado').innerHTML;
+    document.querySelector('#resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+function calcular() {
+    let resultado = document.querySelector('#resultado').innerHTML;
+    document.querySelector('#resultado').innerHTML = eval(resultado);
+}
